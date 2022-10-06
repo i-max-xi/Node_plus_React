e7e11c780151d4e19fa5e3b57b8fe0c2123c2656
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-06 11:19:33 +0000

    settimeout and callbacks

---
+++ TestNode/race.js
@@ -0,0 +1,15 @@
+// setTimeout(() => {
+//     console.log('🐇 finishes!');
+// }, 1000)
+// console.log('🐢 finishes!');
+
+// let myobj = {val :'🐢 finishes!'};
+// console.log(typeof(myobj));
+
+// JSON.stringify(myobj);
+
+// console.log(typeof(myobj));
+
+// setTimeout(() => {
+//     console.log('🐇 finishes!');
+// }, 1000)
---
+++ TestNode/test.js
@@ -0,0 +1,5 @@
+let mission = process.argv[2];
+
+mission === "learn"
+  ? console.log(`to ${mission} is fun`)
+  : console.log(`you should ${mission}`);

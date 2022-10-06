41c311b78a945f94f0bb4bd4a5883783ad63c304
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-06 11:37:23 +0000

    server and app setup

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+  const [backendData, setBackendData] = useState([{}]);
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      {
+        (typeof backendData.users === 'undefined') ? (
+          <p>Loading...</p> 
+        ) : (
+          backendData.users.map((user, index) => (
+            <p key={index}>{user}</p>
+          ))
+        )
+      }
+    </div>
   )
 }
 

ad20b5ad1a862f8b2b26923e92d2e802656aec2b
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-11 04:54:02 +0000

    module exports

---
+++ TestNode/Modules-Example/request.js
@@ -0,0 +1,18 @@
+//you can use module.exports. to export individually
+
+const REQUEST_TIMEOUT = 500;
+function encrypt(data) {
+    return 'encrypted data';
+}
+
+function send (url, data) {
+    const encryptedData = encrypt(data);
+    console.log(`sending ${encryptedData} to ${url}`);
+}
+
+module.exports = {
+    REQUEST_TIMEOUT,
+    send
+}
+
+// console.log(module);
